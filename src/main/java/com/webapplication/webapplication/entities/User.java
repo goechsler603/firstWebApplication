95524161ca51785a597b74f1b2ca4fc0ffb1ddc1
goechsler <oechslergui@gmail.com>
2025-05-13 23:30:04 -0300

    DataBase test

---
+++ src/main/java/com/webapplication/webapplication/entities/User.java
@@ -0,0 +1,39 @@
+package com.webapplication.webapplication.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "userTb")
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+
+
+}
