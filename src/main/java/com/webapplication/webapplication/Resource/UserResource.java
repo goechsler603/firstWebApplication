260f6dea31ef28b338a5d6e88ace9340c4e2b13a
goechsler <oechslergui@gmail.com>
2025-06-04 19:27:07 -0300

    Delete User

--- src/main/java/com/webapplication/webapplication/Resource/UserResource.java
+++ src/main/java/com/webapplication/webapplication/Resource/UserResource.java
@@ -42,4 +42,10 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
